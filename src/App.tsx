e2f47c8f6437016bcfd874715213280363bf37f4
HyeonSeung Lim <hs052929@gmail.com>
2026-09-20 18:51:20 +0900

    feat: add guest browsing soft surfaces and password visibility

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { RealApp } from "./RealApp";
 import "./App.css";
 import "./ux.css";
 import "./viewport-layout.css";
+import "./soft-surfaces.css";
 
 export default function App() {
   return <RealApp />;
